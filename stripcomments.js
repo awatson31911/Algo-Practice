78b106686b3bdb328c3f1981907c9d1d250f13cb
Anthony <awatson31911@gmail.com>
2019-03-25 17:42:18 -0400

    submits first attempt solution with sample test cases

--- stripcomments.js
+++ stripcomments.js
@@ -1,10 +1,11 @@
 function solution(input, markers) {
-    const possibleMarkers = markers.join('|\\');
-    const regExPattern = new RegExp(`(\\s|\\t)*(\\${possibleMarkers}).*`, 'g');
-    
-    return input.replace(regExPattern, '');
-};
-
+    // Join markers by regEx OR operator ("|") and escape character
+        const possibleMarkers = markers.join('|\\'); 
+    // Create new regEx pattern to allow for plugging in marker value dynamically. Also need to add escape character for first marker as it doesn't get escaped in the join function above
+        const regExPattern = new RegExp(`(\\s|\\t)*(\\${possibleMarkers}).*`, 'g');
+        
+        return input.replace(regExPattern, '');
+    };
 
 let str1 = "apples, pears # and bananas\ngrapes\nbananas !apples"
  str1.split(["#", "!"])
