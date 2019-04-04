3d7b780bd96f5f64533dca650d1ed81ecbb17cbc
Anthony <awatson31911@gmail.com>
2019-04-04 15:51:36 -0400

    solves twoStringMerge problem recursively

--- twoStringMerge.js
+++ twoStringMerge.js
@@ -1,3 +1,16 @@
 function twoStringMerge (str1, str2) {
-    
+    //3 Base Cases
+    if (str1.length === 1 && str2.length === 0) {
+        return str1[0];
+    } else if (str1.length === 0 && str2.length === 1) {
+        return str2[0];
+    } else if (str1.length === 1 && str2.length === 1) {
+        return str1[0] + str2[0];
+    } 
+
+    //Only add first element of either string if the string has elements
+    return `${str1.length >=1 ? str1[0] : ''}${str2.length >=1 ? str2[0] : ''}${twoStringMerge(str1.slice(1), str2.slice(1))}`
 }
+
+console.log(twoStringMerge('Hello', 'Worlduh'));
+console.log(twoStringMerge('Hello', 'World'));
