9d870be6c98c96bcc292b449ff856ef9bb39841e
Anthony <awatson31911@gmail.com>
2019-04-11 10:43:39 -0400

    solves maxContiguousSum with dynamic programming - returns maxSum and number[]

--- maxContiguousSum.js
+++ maxContiguousSum.js
@@ -3,26 +3,48 @@
 
 function maxContiguousSum(arr) {
     let maxCount = 0, currCount = 0, currArray = [], maxArr = [];
-    
+
     for (let i = 0; i < arr.length; i++) {
         let num = arr[i];
         currCount = num;
         if (currCount > maxCount) {
             maxCount = currCount;
         }
-        
-        for (let j = i+1; j < arr.length; j++) {
+
+        for (let j = i + 1; j < arr.length; j++) {
             let nextNum = arr[j];
-            
+
             if (currCount > maxCount) {
                 maxCount = currCount;
             }
-            currCount+= nextNum;
+            currCount += nextNum;
         }
     }
-     
+
     return maxCount;
 }
 
+function maxContiguousSum(arr) {
+    let maxSum = 0, currSum = 0, currArray = [], maxArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        //( currSum + num > currSum ) ? currSum += num : currSum = 0;
+        if(currSum + num > currSum) {
+            currSum += num
+            currArray.push(num)
+        } else { 
+            currSum = 0;
+            currArray = [];
+        }
+        if (currSum > maxSum) { 
+            maxSum = currSum;
+            maxArr = currArray;
+        };
+    }
+
+    return [maxArr, maxSum];
+}
+
 maxContiguousSum([3, -4, 8, 7, -10, 19, -3])
-maxContiguousSum([2, -3, -3, 9, -29, 8, -9])
+maxContiguousSum([2, -3, 2, 9, -29, 8, -9])
