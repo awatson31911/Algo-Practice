8054eedcda324d6b1dbd7af9c619efd3ba598bfc
Anthony <awatson31911@gmail.com>
2019-04-12 17:20:31 -0400

    solves twoLowestSum problem using dynamic programming and Typescript

--- twoLowestSum.ts
+++ twoLowestSum.ts
@@ -1,10 +1,28 @@
-function twoLowestSum (numArr: number[] ): number  {
+function twoLowestSum(numArr: number[]): number {
     const numHash = {};
-    let lowestNum = numArr[0], secondLowest = numArr[1];
-    
-    for (const num of numArr) {
-        
+    let lowestNum = numArr[0], nextLowestNum = numArr[0];
+
+    for (let i = 0; i < numArr.length; i++) {
+        let num = numArr[i];
+        if (numHash[num]) {
+            while (numHash[num] != undefined) {
+                num++;
+            }
+        } else {
+            numHash[num] = true;
+        }
+
+        if (num < lowestNum) {
+            lowestNum = num;
+        } else if (num > lowestNum && lowestNum === nextLowestNum) {
+            nextLowestNum = num;
+        } else if (num > lowestNum && num < nextLowestNum) {
+            nextLowestNum = num;
+        }
     }
-    return lowestNum + secondLowest;
- 
- }
+    
+    return lowestNum + nextLowestNum;
+}
+
+twoLowestSum([1, 1, 1, 1, 1, 1])
+twoLowestSum([11, 5, 5, 6, 7, 7])
