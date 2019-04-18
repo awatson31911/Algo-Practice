f01c8a0b9b5afa6b4138e338fd18f365fa279ed8
Anthony <awatson31911@gmail.com>
2019-04-18 07:33:29 -0400

    solves findClosestValueInBst recursively

--- findClosestValueInBst.js
+++ findClosestValueInBst.js
@@ -1,7 +1,26 @@
 function findClosestValueInBst(tree, target) {
-    // Write your code here.
-  }
-  
-  // Do not edit the line below.
-  exports.findClosestValueInBst = findClosestValueInBst;
-  
+    if (tree.left === null && tree.right === null) { return tree.value; }
+
+    const maxDifference = Math.abs(target - tree.value);
+    const maxDifferenceNode = tree.value;
+    const leftValue = (tree.left !== null) && (target < tree.value)
+        ? findClosestValueInBst(tree.left, target)
+        : Infinity;
+    const rightValue = (tree.right !== null) && (target > tree.value)
+        ? findClosestValueInBst(tree.right, target)
+        : Infinity;
+
+    const maxDifferenceRightLeft = Math.abs(target - rightValue) < Math.abs(target - leftValue)
+        ? Math.abs(target - rightValue)
+        : Math.abs(target - leftValue);
+    const maxDifferenceNodeRightLeft = maxDifferenceRightLeft === Math.abs(target - rightValue)
+        ? rightValue
+        : leftValue;
+
+    if (maxDifferenceRightLeft <= maxDifference) { return maxDifferenceNodeRightLeft; }
+
+    return maxDifferenceNode;
+}
+
+// Do not edit the line below.
+exports.findClosestValueInBst = findClosestValueInBst;
