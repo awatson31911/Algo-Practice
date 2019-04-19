1bd63fa82937454d6ea45f73b1e94e837ee7f45b
Anthony <awatson31911@gmail.com>
2019-04-19 18:09:10 -0400

    adds to bubbleSort, still failing some test cases

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,20 +1,30 @@
-let arr = [10, 4, 2, 6, 3, 7, 2, 4];
 function bubbleSort(numbersArray) {
-    numbersArray.forEach( (number, currentIndex) => {
-        let nextIndex = currentIndex + 1;
-        while (nextIndex < numbersArray.length) {
-            const nextNumber = numbersArray[nextIndex];
-            if(number > nextNumber) {
-                numbersArray[currentIndex] = nextNumber;
-                numbersArray[nextIndex] = number;
-                currentIndex++;
+    for (let index = 0; index < numbersArray.length; index++) {
+        let hasNoSwaps = false;
+        while (hasNoSwaps === false) {
+            hasNoSwaps = true;
+            let currentIndex = index;
+            const number = numbersArray[currentIndex];
+            let nextIndex = currentIndex + 1;
+
+            while (nextIndex < numbersArray.length) {
+                const nextNumber = numbersArray[nextIndex];
+                if (number > nextNumber) {
+                    numbersArray[currentIndex] = nextNumber;
+                    numbersArray[nextIndex] = number;
+                    currentIndex++;
+                    hasNoSwaps = true;
+                }
+                nextIndex++;
             }
-            nextIndex++;
         }
-    });
+    }
+    return numbersArray;
 }
 
 // Do not edit the line below.
-//exports.bubbleSort = bubbleSort;
+exports.bubbleSort = bubbleSort;
 
-bubbleSort(arr);
+let testArr = [10, 4, 2, 6,2, 3, 3];
+bubbleSort(testArr);
+console.log(bubbleSort(testArr));
