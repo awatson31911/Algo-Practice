34d57b27a08d2a2ddd6ed1f5b86000c042562380
Anthony <awatson31911@gmail.com>
2019-04-22 09:31:20 -0400

    solves bubbleSort iteratively

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,21 +1,14 @@
 function bubbleSort(numbersArray) {
-    for (let index = 0; index < numbersArray.length; index++) {
-        let hasNoSwaps = false;
-        while (hasNoSwaps === false) {
-            hasNoSwaps = true;
-            let currentIndex = index;
-            const number = numbersArray[currentIndex];
-            let nextIndex = currentIndex + 1;
 
-            while (nextIndex < numbersArray.length) {
-                const nextNumber = numbersArray[nextIndex];
-                if (number > nextNumber) {
-                    numbersArray[currentIndex] = nextNumber;
-                    numbersArray[nextIndex] = number;
-                    currentIndex++;
-                    hasNoSwaps = true;
-                }
-                nextIndex++;
+    for (let i = 0; i < numbersArray.length; i++) {
+        for (let leftIndex = 0; leftIndex + 1 < numbersArray.length; leftIndex++) {
+            const rightIndex = leftIndex + 1;
+            const leftNumber = numbersArray[leftIndex];
+            const rightNumber = numbersArray[rightIndex];
+            if (leftNumber > rightNumber) {
+                numbersArray[leftIndex] = rightNumber;
+                numbersArray[rightIndex] = leftNumber;
+
             }
         }
     }
@@ -25,6 +18,5 @@ function bubbleSort(numbersArray) {
 // Do not edit the line below.
 exports.bubbleSort = bubbleSort;
 
-let testArr = [10, 4, 2, 6,2, 3, 3];
+let testArr = [10, 4, 2, 6, 2, 3, 3];
 bubbleSort(testArr);
-console.log(bubbleSort(testArr));
