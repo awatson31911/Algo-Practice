d222b06da1268c673ed22912debf5bdac2679b4d
Anthony <awatson31911@gmail.com>
2019-04-26 10:06:44 -0400

    solves fourTargetSum iteratively

--- fourTargetSum.js
+++ fourTargetSum.js
@@ -1,3 +1,27 @@
-function fourTargetSum (inputArr, targetNum) {
-    
+function findArrayQuadruplet(inputArr, targetSum) {
+    if (inputArr.length < 4) { return []; }
+
+    inputArr.sort((a, b) => a - b);
+
+    for (let i = 0; i < inputArr.length - 3; i++) {
+
+        for (let j = i + 1; j < inputArr.length - 2; j++) {
+            let leftPointerIdx = j + 1;
+            let rightPointerIdx = inputArr.length - 1;
+
+            while (leftPointerIdx < rightPointerIdx) {
+                const currSum = inputArr[i] + inputArr[j] + inputArr[leftPointerIdx] + inputArr[rightPointerIdx];
+
+                if (currSum === targetSum) {
+                    return [inputArr[i], inputArr[j], inputArr[leftPointerIdx], inputArr[rightPointerIdx]];
+                } else if (currSum < targetSum) {
+                    leftPointerIdx++;
+                } else {
+                    rightPointerIdx--;
+                }
+            }
+        }
+    }
+
+    return [];
 }
