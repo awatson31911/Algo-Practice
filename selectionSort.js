760eaef5158c27d2183de74e274b582d47e3b346
Anthony <awatson31911@gmail.com>
2019-09-15 15:03:56 -0400

    adds selection sort solution

--- selectionSort.js
+++ selectionSort.js
@@ -1,5 +1,25 @@
 function selectionSort(numberArray) {
-   
+    if (numberArray.length === 1 || numberArray.length === 0) return numberArray;
+    
+    let smallestNumIdx; 
+    for (let i = 0; i < numberArray.length - 1; i++) {
+        let smallestNumIdx = i;
+
+        for (let j = i + 1; j < numberArray.length; j++) {
+            if (numberArray[j] < numberArray[smallestNumIdx]) smallestNumIdx = j;
+        }
+
+        swap(smallestNumIdx, i, numberArray)
+    }
+
+    return numberArray;
+
+
+    function swap(idx1, idx2, array) {
+        const buffer = array[idx1];
+        array[idx1] = array[idx2];
+        array[idx2] = buffer;
+    }
 }
 
 
@@ -7,6 +27,6 @@ function selectionSort(numberArray) {
 exports.selectionSort = selectionSort;
 
 
-const testArr = [10, 4, 2, 6, 2, 3, 3];
+const testArr = [10, 4, 2, 6, 1,2, 3, 3];
 selectionSort(testArr);
 console.log(selectionSort(testArr));
