4fe4a2a3215656f080b89c57f059f9206adcae80
Anthony <awatson31911@gmail.com>
2019-10-06 12:54:44 -0400

    adds updated solution for frequency queries

--- hackerRank/frequencyQueries.js
+++ hackerRank/frequencyQueries.js
@@ -2,36 +2,33 @@ function freqQuery(queries) {
   const numMap = {};
   const freqMap = {}
   const queryResults = [];
-  let numCount;
+  let currCount;
 
   for (let i = 0; i < queries.length; i++) {
-      const queryType = queries[i][0];
-      const queryVal = queries[i][1];
+    const queryType = queries[i][0];
+    const queryVal = queries[i][1];
+    currCount = numMap[queryVal] ? numMap[queryVal] : 0;
 
-      switch (queryType) {
-          case 1:
-              numMap[queryVal] ? numMap[queryVal]++ : numMap[queryVal] = 1;
-              numCount = numMap[queryVal];
+    switch (queryType) {
+      case 1:
+        freqMap[currCount + 1] = freqMap[currCount + 1] ? freqMap[currCount + 1] + 1 : 1;
+        if (freqMap[currCount]) freqMap[currCount]--;
+        numMap[queryVal] = numMap[queryVal] ? numMap[queryVal] + 1 : 1;
+        break;
+      case 2:
+        if (freqMap[currCount - 1]) freqMap[currCount - 1]++;
+        if (freqMap[currCount]) freqMap[currCount]--;
+        if (numMap[queryVal]) numMap[queryVal] = Math.max(currCount - 1, 0)
+        break;
+      case 3:
+        if (freqMap[queryVal]) {
+          queryResults.push(1);
+        } else {
+          queryResults.push(0);
+        }
+        break;
+    }
 
-              freqMap[numCount] ? freqMap[numCount]++ : freqMap[numCount] = 1;
-              freqMap[numCount - 1]--;
-              break;
-          case 2:
-              numCount = numMap[queryVal];
-              freqMap[numCount]--;
-              if (numCount - 1) freqMap[numCount - 1]++;
-              if (numMap[queryVal] > 0) numMap[queryVal]--;
-              break;
-          case 3:
-              if (freqMap[queryVal]) {
-                  queryResults.push(1);
-              } else {
-                  queryResults.push(0);
-              }
-              break;
-      }
-
-      console.log(numMap, freqMap, queryResults)
   }
 
   return queryResults;
